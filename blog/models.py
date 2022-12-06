557dba33adb784d2c07eea251858219db65f2c70
SungJoon Byun <byu4n07@naver.com>
2022-12-06 17:20:59 +0900

    대문 페이지 수정하기

--- blog/models.py
+++ blog/models.py
@@ -64,6 +64,12 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1367/f9164fe9a2fc6932/svg/{self.author.email}'
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
