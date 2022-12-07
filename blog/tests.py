1e3c8c5acbdc44cbd91f010b403cc3576529069c
SungJoon Byun <byu4n07@naver.com>
2022-12-07 10:44:36 +0900

    자기소개 페이지에 내용 채우기

--- blog/tests.py
+++ blog/tests.py
@@ -142,7 +142,7 @@ class TestView(TestCase):
         self.assertIn('Blog', navbar.text)
         self.assertIn('About Me', navbar.text)
 
-        logo_btn = navbar.find('a', text='Do it Django')
+        logo_btn = navbar.find('a', text='다락방')
         self.assertEqual(logo_btn.attrs['href'], '/')
 
         home_btn = navbar.find('a', text='Home')
